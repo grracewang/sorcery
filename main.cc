1cf081cdcf5726c6a460ddcdf61bea597790dd8e
Grace <g44wang@uwaterloo.ca>
2024-07-23 15:50:15 -0400

    fixed main

--- main.cc
+++ main.cc
@@ -10,6 +10,7 @@
 #include "concrete spells/blizzard.h"
 #include "concrete spells/raisedead.h"
 
+#include <utility>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -42,9 +43,6 @@ bool convertOp(const string &command, Op &op, bool testing) {
 
 
 int main(int argc, char *argv[]) {
-    Op op;
-
-    int i = 0;
 
     // filenames
     string initFile = "";
@@ -58,6 +56,7 @@ int main(int argc, char *argv[]) {
     bool setDeck2 = false;
     bool graphics = false;
 
+    int i = 0;
     // set up arguments
     while (i < argc) {
 		string arg = argv[i]; 
@@ -97,7 +96,7 @@ int main(int argc, char *argv[]) {
     vector<Player*> players;
 
     // getting both player's names if not provided (but didn't implement the if not provided part)
-    for (int i = 1; i <= NUM_PLAYERS; i++) {
+    for (i = 1; i <= NUM_PLAYERS; i++) {
         string name;
 
         cout << "What's player " << i << "'s name?" << endl;
@@ -140,167 +139,142 @@ int main(int argc, char *argv[]) {
     // } 
     cout << "Decks shuffled successfully!" << endl;
 
-    i = 0;
+    Op op;
     int curr = 0;
     int next = 1;
     while (true) {
-        cout << "Type your command: " << endl;
-        if (curr == players.size()) { // helps us count whose turn it is
-            curr = 0; 
-            next = 1;
-        } else if (next == players.size()) {
-            next = 0;
-        }
-        string command;
+        while (true) {
+            string command;
 
-        cin >> command;
-        if (cin.fail()) break;
-        if (!convertOp(command, op, testing)) {
-            cerr << "Invalid command!" << endl;
-            continue;
-        }
-
-        // if (op == Op::QUIT) break;
-
-        switch (op) {
-            case Op::HELP:
-            {
-                // outputs the help message in help.txt
-                ifstream f{"help.txt"};
-                string str;
-                while (getline(f, str)) {
-                    cout << str << endl;
-                }
+            cout << "Type your command: " << endl;
+            cin >> command;
+            if (cin.fail()) break;
+            if (!convertOp(command, op, testing)) {
+                cerr << "Invalid command!" << endl;
+                continue;
             }
-            break;
 
-            case Op::END:
-            {
+            if (op == Op::QUIT) {
+                cout << "Quitting the game" << endl;
+                break;
+            }
+            
+            if (op == Op::END) {
                 cout << "Command: end" << endl;
                 // trigger all spells and rituals at the end
+                break; // break out of the "turn" while loop
             }
-            break;
 
-            case Op::DRAW:
-            {
-                players[curr]->draw();
-                cout << "Command: Card drawn for player " << curr + 1 << endl;
-                // cerr << "Hand size is: " << players[curr]->getHand().size() << endl;
-                for (int i = 0; i < players[curr]->getHand().size(); i++) {
-                    cout << "Card drawn is: " << players[curr]->getHand()[i]->getName() << endl;
+            switch (op) {
+                case Op::HELP:
+                {
+                    // outputs the help message in help.txt
+                    ifstream f{"help.txt"};
+                    string str;
+                    while (getline(f, str)) {
+                        cout << str << endl;
+                    }
                 }
-            }
-            break;
+                break;
 
-            case Op::DISCARD:
-            {
-                cin >> i;
-                players[curr]->discard(i);
-                cout << "Command: Discarded card " << i << " of player " << curr + 1 << endl;
-            }
-            break;
-
-            case Op::ATTACK:
-            {
-                int j;
-                cin >> i;
-                cin >> j;
-                if (cin.fail()) {
-                    cin.clear();
-                    cout << "Command: attack minion" << i << endl;
-                } else {
-                    cout << "Command: attack minion" << i << j << endl;
+                case Op::DRAW:
+                {
+                    players[curr]->draw();
+                    cout << "Command: Card drawn for player " << curr + 1 << endl;
+                    // cerr << "Hand size is: " << players[curr]->getHand().size() << endl;
+                    for (int i = 0; i < players[curr]->getHand().size(); i++) {
+                        cout << "Card drawn is: " << players[curr]->getHand()[i]->getName() << endl;
+                    }
                 }
-            }
-            break;
+                break;
 
-            case Op::PLAY:
-            {
-                int p; // t-th card owned by player
-                cin >> i;
-                cin >> p;
-                if (cin.fail()) {
-                    cin.clear();
-                    // check if card played is minion, if it's a minion we call all the spells/rituals minion related
-                    if (players[curr]->getHand()[i]->getType() == "Minion") { // type is minion
-                        players[curr]->placeMinion(i);
-                    } else if (players[curr]->getHand()[i]->getType() == "Ritual") { // type is ritual
-                        players[curr]->placeRitual(i);
-                    } else if (players[curr]->getHand()[i]->getName() == "Blizzard") {
-                        Blizzard* card = dynamic_cast<Blizzard*>(players[curr]->getHand()[i]);
-                        if (card) {
-                            card->activate(players[curr], players[next]);
-                            cout << "Played Blizzard." << endl;
+                case Op::DISCARD:
+                {
+                    cin >> i;
+                    players[curr]->discard(i);
+                    cout << "Command: Discarded card " << i << " of player " << curr + 1 << endl;
+                }
+                break;
+
+                case Op::ATTACK:
+                {
+                    int j;
+                    cin >> i;
+                    cin >> j;
+                    if (cin.fail()) {
+                        cin.clear();
+                        cout << "Command: attack minion" << i << endl;
+                    } else {
+                        cout << "Command: attack minion" << i << j << endl;
+                    }
+                }
+                break;
+
+                case Op::PLAY:
+                {
+                    int p; // t-th card owned by player
+                    cin >> i;
+                    cin >> p;
+                    if (cin.fail()) {
+                        cin.clear();
+                        // check if card played is minion, if it's a minion we call all the spells/rituals minion related
+                        if (players[curr]->playCard(i, players[curr], players[next])) {
+                            cout << "Command: Played card " << i << endl;
                         } else {
-                            cout << "Cannot call command on this card." << endl;
-                        }
-                    } else if (players[curr]->getHand()[i]->getName() == "Raise Dead") {
-                        RaiseDead* card = dynamic_cast<RaiseDead*>(players[curr]->getHand()[i]);
-                        if (card) {
-                            card->activate(players[curr]);
-                            cout << "Played Raise Dead." << endl;
-                        } else {
-                            cout << "Cannot call command on this card." << endl;
+                            cout << "Card can't be played." << endl;
                         }
                     } else {
-                        cout << "Cannot call command on this card." << endl;
+                        char t; // t can only be 30, 31, 32, 33, 34, 114 (= r)
+                        cin >> t;
+                        //if (players[curr]->playCard(i, players[next], t)) {
+                        if (true) {
+                        cout << "Command: Played card " << i << p << t << endl; 
+                        } else {
+                            cout << "Card can't be played." << endl;
+                        }
+                        
                     }
-                } else {
-                    char t; // t can only be 30, 31, 32, 33, 34, 114 (= r)
-                    cin >> t;
-                    //if (players[curr]->playCard(i, players[next], t)) {
-                    if (true) {
-                       cout << "Command: Played card " << i << p << t << endl; 
-                    } else {
-                        cout << "Card can't be played." << endl;
-                    }
-                    
                 }
-            }
-            break;
+                break;
 
-            case Op::USE:
-            {
+                case Op::USE:
+                {
 
-                cin >> i;
-                cout << "Command: use" << i << endl;    
-            }
-            break;
+                    cin >> i;
+                    cout << "Command: use" << i << endl;    
+                }
+                break;
 
-            case Op::INSPECT:
-            {
-                cin >> i;
-                cout << "Command: inspect" << i << endl;
-            }
-            break;
+                case Op::INSPECT:
+                {
+                    cin >> i;
+                    cout << "Command: inspect" << i << endl;
+                }
+                break;
 
-            case Op::HAND:
-            {
-                cout << "Command: hand" << endl;
-            }
-            break;
+                case Op::HAND:
+                {
+                    cout << "Command: hand" << endl;
+                }
+                break;
 
-            case Op::BOARD:
-            {
-                cout << "Command: board" << endl;
-            } 
-            break;
+                case Op::BOARD:
+                {
+                    cout << "Command: board" << endl;
+                } 
+                break;
+            } // switch
 
-            case Op::QUIT:
-            {
-                cout << "Quitting the game" << endl;
-                return 1;
-            }
-            break;
-            // case Op::INVALID_COMMAND:
-            // {
+        } // while--turn loop (commands per turn)
 
-            // }
-        } //switch
+        swap(curr, next);
 
-        ++curr; // next player's turn
-        ++next;
-    } //while
+        // force end game
+        if (op == Op::QUIT || cin.fail()) break;
+
+        // if winning condition -> print win message, break from game loop
+
+    } // while--game loop (switch between players)
 
     // delete everything created on the stack
 } // main
