b3277a0f8f0e085a569d27b2ad4e50e8b5d23a56
Anne <anne12080901@gmail.com>
2024-07-24 19:53:07 -0400

    fixed raise dead

--- concrete spells/raisedead.h
+++ concrete spells/raisedead.h
@@ -3,19 +3,17 @@
 
 #include "../spell.h"
 #include "player.h"
-#include <sstream>
-#include <iostream>
-#include "string.h"
+#include "../concrete minions/changestat.h"
 class RaiseDead: public Spell {
     public:
-        explicit RaiseDead(): Spell{"Raise Dead", "Resurrect the top minion in your graveyard and set its defence to 1", 1} {}
+        RaiseDead(): Spell{"Raise Dead", "Resurrect the top minion in your graveyard and set its defence to 1", 1} {}
         void activate(Player* owner) {
             Minion* target = owner->revive();
-            int i = -1 * target->getDef() + 1;
-            std::istringstream iss(i);
-            std::string str;
-            iss >> str;
-            target = new ChangeStat{target, "", str, 0, 0, false, false};
+            if (target->getDef() <= 0) {
+                target = new ChangeStat{target, "", ChangeStat::toString('+', -1 * target->getDef() + 1), 0, 0, false, false};
+            } else {
+                target = new ChangeStat{target, "", ChangeStat::toString('+', 1 * target->getDef() + 1), 0, 0, false, false};
+            }
             owner->addToSummoned(target);
         }
 };
