a17d4de7a59d6681faec143b043abc8425218e8b
Grace <g44wang@uwaterloo.ca>
2024-07-24 20:23:14 -0400

    changed raise dead activate function

--- concrete spells/blizzard.h
+++ concrete spells/blizzard.h
@@ -7,11 +7,11 @@
 class Blizzard: public Spell {
     public:
         explicit Blizzard(): Spell{"Blizzard", "Deal 2 damage to all minions", 3} {}
-        void activate(Player* owner, Player* target) {
+        void activate(Player *owner, Player *enemy) {
             for (auto minion: owner->getSummoned()) {
                 minion = new ChangeStat{minion, "", "-2", 0, 0, false, false};
             } 
-            for (auto minion: target->getSummoned()) {
+            for (auto minion: enemy->getSummoned()) {
                 minion = new ChangeStat{minion, "", "-2", 0, 0, false, false};
             }
         }
--- concrete spells/raisedead.h
+++ concrete spells/raisedead.h
@@ -7,14 +7,16 @@
 class RaiseDead: public Spell {
     public:
         RaiseDead(): Spell{"Raise Dead", "Resurrect the top minion in your graveyard and set its defence to 1", 1} {}
-        void activate(Player* owner) {
+        bool activate(Player *owner, Player *enemy) {
             Minion* target = owner->revive();
-            if (target->getDef() <= 0) {
-                target = new ChangeStat{target, "", ChangeStat::toString('+', -1 * target->getDef() + 1), 0, 0, false, false};
-            } else {
-                target = new ChangeStat{target, "", ChangeStat::toString('+', 1 * target->getDef() + 1), 0, 0, false, false};
+            if (target == nullptr || owner->getSummoned().size()) {
+                cout << "You cannot use this card." << endl
+                     << "Either there is nothing in the graveyard or you already summoned 5 minions." << endl;
+                return false;
             }
+            target = new ChangeStat{target, "", ChangeStat::toString('+', -target->getDef() + 1), 0, 0, false, false};
             owner->addToSummoned(target);
+            return true;
         }
 };
 #endif
