ee8e667affa87f025d4acb51c125831614e5de76
Grace <g44wang@uwaterloo.ca>
2024-07-25 14:51:47 -0400

    disenchant

--- concrete spells/disenchant.h
+++ concrete spells/disenchant.h
@@ -10,16 +10,7 @@ class Disenchant: public Spell {
         bool activate(Player *target, Player *other, int t) override {
             Minion* minion = target->getSummonedMinion(t);
             minion = Minion::removeTopEnchantment(minion);
-            
-            // if the top enchantment is +2/+2 or *2/*2 then we wrap another change stat that divides by 2
-            // Enchantment* removed = minion->removeEnchantment(minion->getEnchantments().size());
-            // if (removed->getName() == "Giant Strength") {
-            //     minion = new ChangeStat{minion, "-2", "-2", 0, 0, false, false};
-            // } else if (removed->getName() == "Enrage") {
-            //     minion = new ChangeStat{minion, '*', 0.5, 0.5, 0};
-            // } else if (removed->getName() == "Magic Fatigue") {
-            //     minion = new ChangeStat{minion, '+', 0, 0, -2};
-            // }
+            target->replaceMinion(t, minion);
             return true;
         }
 };
