3d4d10c3a236a844d77f7441cb364de4c201fa03
Anne <anne12080901@gmail.com>
2024-07-24 23:24:53 -0400

    .

--- concrete minions/fireelemental.h
+++ concrete minions/fireelemental.h
@@ -8,4 +8,18 @@ class FireElemental : public Minion {
         void activate(Card *t) {};
         int getAtk() const override { return 2; }
         int getDef() const override { return 2; }
+         // normal getters (not changed by decorators)
+		string getName() const override;
+		string getType() const override;
+		int getCost() const override;
+        bool isEnchantment() const override;
+
+		// getters changed by decorators
+        int getAtk() const override;
+		int getDef() const override;
+		int getBeginActions() const override;
+        int getActCost() const override;
+		void addAbility(Player*) override; // called when bone golem is summoned
+		vector<Spell*> getSpells() const override;
+		// vector<Ritual*> getRituals() const override;
 };
